c3df302e17d8efa2a467e8c894c771034fea56ac
skrk0ss@gmail.com <skrk0ss@gmail.com>
2016-02-15 17:23:09 +0900

    test

---
+++ study-wf/web/src/main/java/org/springframework/samples/wf/web/Application.java
@@ -0,0 +1,19 @@
+package org.springframework.samples.wf.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.sample.wf.core.util.StringUtils;
+
+@SpringBootApplication
+public class Application {
+
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+
+    logger.info("test:" + StringUtils.isBlank("test"));
+  }
+}
